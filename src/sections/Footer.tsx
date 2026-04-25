6b7135f3836d3247b910140e2744aec8407361b5
navgr2001 <naveenshalom2001@gmail.com>
2026-04-25 23:14:28 +0530

    link

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,16 +1,22 @@
-import { Container } from '../components/Container';
-import { designer, navItems } from '../data/content';
+import { Link } from "react-router-dom";
+import { Container } from "../components/Container";
+import { designer, navItems } from "../data/content";
 
 export function Footer() {
   return (
     <footer className="border-t border-white/10 py-8">
       <Container className="flex flex-col gap-5 text-sm text-zinc-500 md:flex-row md:items-center md:justify-between">
         <p>© 2026 {designer.name}. All rights reserved.</p>
+
         <div className="flex flex-wrap gap-4">
           {navItems.map((item) => (
-            <a key={item.href} href={item.href} className="transition hover:text-white">
+            <Link
+              key={item.path}
+              to={item.path}
+              className="transition hover:text-white"
+            >
               {item.label}
-            </a>
+            </Link>
           ))}
         </div>
       </Container>

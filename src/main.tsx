29af98d556d69e43552d7f7d408fcc230402ab2a
navgr2001 <naveenshalom2001@gmail.com>
2026-04-25 23:30:10 +0530

    security

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import emailjs from "@emailjs/browser";
 import App from "./App";
 import "./styles.css";
 
+const emailJsPublicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY as
+  | string
+  | undefined;
+
+if (emailJsPublicKey) {
+  emailjs.init({
+    publicKey: emailJsPublicKey,
+    blockHeadless: true,
+    limitRate: {
+      id: "portfolio-contact-form",
+      throttle: 10000,
+    },
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
